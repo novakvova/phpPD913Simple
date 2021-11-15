750d47d2b6665f1a1481ca5afd519e55c31103f4
novakvova <novakvova@gmail.com>
2021-11-15 17:55:31 +0200

    Simple app

---
+++ create.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <title>Додати новину</title>
+</head>
+<?php
+
+?>
+<body>
+
+<?php include "navbar.php"; ?>
+
+    <h1>Додати новину</h1>
+
+
+    <script src="/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
