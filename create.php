76f1592c45bbc18e7439f69f4ffa93746441d1fc
novakvova <novakvova@gmail.com>
2021-11-17 16:04:55 +0200

    Create and show data

--- create.php
+++ create.php
@@ -1,3 +1,19 @@
+<?php
+if($_SERVER["REQUEST_METHOD"]=="POST") {
+//    echo "<h2>name = ".$_POST['name']."</h2>";
+    $name=$_POST['name'];
+    $description=$_POST['description'];
+    $image=$_POST['image'];
+
+    $conn = new PDO("mysql:host=localhost;dbname=local.pd913.com", "root", "");
+    $sql = "INSERT INTO `news` (`name`, `description`,`image`) VALUES (?, ?, ?);";
+    $conn->prepare($sql)->execute([$name,$description,$image]);
+    header("Location: /");
+    exit();
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,9 +30,27 @@
 
 <?php include "navbar.php"; ?>
 
+<div class="container">
     <h1>Додати новину</h1>
+    <form method="post">
+        <div class="mb-3"">
+            <label for="name" class="form-label">Назва</label>
+            <input type="text" class="form-control" id="name" name="name" placeholder="Enter name">
+        </div>
+        <div class="mb-3" class="form-label">
+            <label for="description">Опис</label>
+            <textarea class="form-control" rows="10" cols="35" id="description" name="description" placeholder="Enter email"></textarea>
+        </div>
+        <div class="mb-3" class="form-label">
+            <label for="image">Фото</label>
+            <input type="text" class="form-control" id="image" name="image" placeholder="Enter image">
+        </div>
+        <button type="submit" class="btn btn-primary">Зберегти</button>
+    </form>
+
+</div>
 
 
-    <script src="/js/bootstrap.bundle.min.js"></script>
+<script src="/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -8,16 +8,44 @@
     <title>Новини</title>
 </head>
 <?php
+$conn = new PDO("mysql:host=localhost;dbname=local.pd913.com", "root", "");
+$reader = $conn->query("SELECT * FROM news");
 
 ?>
 <body>
 
 <?php include "navbar.php"; ?>
-
-
+<div class="container">
     <h1>Головна сторінка</h1>
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Description</th>
+            <th scope="col">Image</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+        foreach ($reader as $row) {
+            echo "
+        <tr>
+            <td>{$row['id']}</td>
+            <td>{$row['name']}</td>
+            <td>{$row['description']}</td>
+            <td>
+                <img src='/images/{$row['image']}' alt='Bear' width='100'/>
+                
+            </td>
+        </tr>
+        ";
+        }
+        ?>
+        </tbody>
+    </table>
+</div>
 
-
-    <script src="/js/bootstrap.bundle.min.js"></script>
+<script src="/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
